c783100221b28d9e10309499e7e94ea5c5f3c7e6
Daniel Kulichkov <danchick03@gmail.com>
2026-05-18 20:33:23 +0300

    feat: create projects grid

---
+++ src/content.config.ts
@@ -0,0 +1,22 @@
+import { defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
+
+const projects = defineCollection({
+  loader: glob({ base: "./src/content/projects", pattern: "**/*.md" }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      year: z.string().optional(),
+      role: z.string().optional(),
+      liveLink: z.string().optional(),
+      sourceCodeLink: z.string().optional(),
+      technologies: z
+        .string()
+        .transform((val) => val.split(",").map((skill) => skill.trim())),
+      heroImage: z.optional(image()),
+    }),
+});
+
+export const collections = { projects };
---
+++ src/content/projects/sofile.md
@@ -0,0 +1,9 @@
+---
+title: Sofile
+description: Plugin for connecting your cloud providers to Adobe products
+technologies: Angular, Adobe CEP, Go, Node.js, AWS CDK
+year: '2026'
+role: Lead engineer
+liveLink: https://www.sofile.io/
+heroImage: '../../assets/projects/sofile_post.jpg'
+---
